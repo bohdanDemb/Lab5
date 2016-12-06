90c5f055809e9b60145b05e5b0d452bd1efdecae
DembitskyiBohdan <bodik_net@gmail.com>
2016-12-06 17:04:57 +0200

    Commit2

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String args[]){
-		System.out.println("Hello world");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("¬вед≥ть ≥м`€ користувача");
+		String userName = sc.next();
+		System.out.println("Hello world!, " + userName);
 	}
 }
